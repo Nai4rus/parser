0f430bf087a634e17016d87a6dcafbaa23f013cf
jexme <alexeis@wmstudio.su>
2020-10-31 17:01:58 +0400

    fix bugs

--- components/parser/news/FishnewsRuParser.php
+++ components/parser/news/FishnewsRuParser.php
@@ -8,6 +8,7 @@ use app\components\helper\nai4rus\PreviewNewsDTO;
 use app\components\parser\NewsPost;
 use DateTimeImmutable;
 use DateTimeZone;
+use DOMElement;
 use RuntimeException;
 use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\DomCrawler\UriResolver;
@@ -75,15 +76,24 @@ class FishnewsRuParser extends AbstractBaseParser
 
         $newsPageCrawler = new Crawler($newsPage);
         $contentCrawler = $newsPageCrawler->filter('article.news');
-        $this->removeDomNodes($contentCrawler, '//time');
-        $this->removeDomNodes($contentCrawler, '//h1[1]');
-        $this->removeDomNodes($contentCrawler, '//strong[contains(text(),"Самые оперативные новости читайте в")]/parent::p/following-sibling::*');
-        $this->removeDomNodes($contentCrawler, '//strong[contains(text(),"Больше новостей читайте в")]/parent::p/following-sibling::*');
-        $this->removeDomNodes($contentCrawler, '//strong[contains(text(),"Самые оперативные новости читайте в")]/parent::p');
-        $this->removeDomNodes($contentCrawler, '//strong[contains(text(),"Больше новостей читайте в")]/parent::p');
+        if ($contentCrawler->count()) {
+            $this->removeDomNodes($contentCrawler, '//time');
+            $this->removeDomNodes($contentCrawler, '//h1[1]');
+            $this->removeDomNodes($contentCrawler, '//strong[contains(text(),"Самые оперативные новости читайте в")]/parent::p/following-sibling::*');
+            $this->removeDomNodes($contentCrawler, '//strong[contains(text(),"Больше новостей читайте в")]/parent::p/following-sibling::*');
+            $this->removeDomNodes($contentCrawler, '//strong[contains(text(),"Самые оперативные новости читайте в")]/parent::p');
+            $this->removeDomNodes($contentCrawler, '//strong[contains(text(),"Больше новостей читайте в")]/parent::p');
+        } else {
+            $previewNewsDTO->setTitle($newsPageCrawler->filter('head > title')->text());
+            $contentCrawler = $newsPageCrawler->filterXPath('//*[contains(concat(" ",normalize-space(@class)," ")," content ")] | //div[contains(@class,"photos-list")]');
+            $descriptionCrawler = $newsPageCrawler->filter('.anons');
+            if ($this->crawlerHasNodes($descriptionCrawler)) {
+                $description = Text::trim($this->normalizeSpaces($descriptionCrawler->text()));
+            }
+        }
 
         $image = null;
-        $mainImageCrawler = $contentCrawler->filterXPath('//a[contains(@class,"interviewee-photo")]/img[1]/parent::a');
+        $mainImageCrawler = $newsPageCrawler->filterXPath('//a[contains(@class,"interviewee-photo")]/img[1]/parent::a');
         if ($this->crawlerHasNodes($mainImageCrawler)) {
             $image = $mainImageCrawler->attr('href');
             $this->removeDomNodes($contentCrawler, '//a[contains(@class,"interviewee-photo")]/img[1]/parent::a');
@@ -100,7 +110,9 @@ class FishnewsRuParser extends AbstractBaseParser
             $previewNewsDTO->setImage($this->encodeUri($image));
         }
 
-        $description = $this->getDescriptionFromContentText($contentCrawler);
+        if (!$description) {
+            $description = $this->getDescriptionFromContentText($contentCrawler);
+        }
 
         if ($description && $description !== '') {
             $previewNewsDTO->setDescription($description);
@@ -111,6 +123,16 @@ class FishnewsRuParser extends AbstractBaseParser
         $newsPostItemDTOList = $this->parseNewsPostContent($contentCrawler, $previewNewsDTO);
 
         return $this->factoryNewsPost($previewNewsDTO, $newsPostItemDTOList);
+    }
+
+    protected function getImageLinkFromNode(DOMElement $node): string
+    {
+        $parentNode = $node->parentNode;
+        if ($parentNode instanceof DOMElement && str_contains($parentNode->getAttribute('class'), 'photo-gallery')) {
+            return $parentNode->getAttribute('href');
+        }
+
+        return $node->getAttribute('src');
     }
 
     private function getDescriptionFromContentText(Crawler $crawler): ?string
