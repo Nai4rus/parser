7a2e829eb6678eab9818bc995c821b02f61eb4b1
Nai <andrei@wmstudio.su>
2020-11-11 17:33:53 +0300

    fix: убрал дублирование изображений

--- components/parser/news/MediaLeaksRuParser.php
+++ components/parser/news/MediaLeaksRuParser.php
@@ -7,6 +7,7 @@ use app\components\helper\nai4rus\PreviewNewsDTO;
 use app\components\parser\NewsPost;
 use DateTimeImmutable;
 use DateTimeZone;
+use DOMElement;
 use RuntimeException;
 use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\DomCrawler\UriResolver;
@@ -70,19 +71,14 @@ class MediaLeaksRuParser extends AbstractBaseParser
         $newsPageCrawler = new Crawler($newsPage);
         $newsPostCrawler = $newsPageCrawler->filterXPath('//article[@itemprop="articleBody"]');
 
-        $image = null;
-        $mainImageCrawler = $newsPageCrawler->filterXPath('//meta[@property="og:image"]')->first();
-        if ($this->crawlerHasNodes($mainImageCrawler)) {
-            $image = $mainImageCrawler->attr('content');
-        }
-        if ($image !== null && $image !== '') {
-            $previewNewsItem->setImage(UriResolver::resolve($image, $uri));
-        }
-
-        // $descriptionCrawler = $newsPostCrawler->filterXPath('//h2');
-        // if ($this->crawlerHasNodes($descriptionCrawler) && $descriptionCrawler->text() !== '') {
-        //     $previewNewsItem->setDescription($descriptionCrawler->text());
-        // }
+//        $image = null;
+//        $mainImageCrawler = $newsPageCrawler->filterXPath('//meta[@property="og:image"]')->first();
+//        if ($this->crawlerHasNodes($mainImageCrawler)) {
+//            $image = $mainImageCrawler->attr('content');
+//        }
+//        if ($image !== null && $image !== '') {
+//            $previewNewsItem->setImage(UriResolver::resolve($image, $uri));
+//        }
 
         $contentCrawler = $newsPostCrawler;
 
@@ -91,5 +87,29 @@ class MediaLeaksRuParser extends AbstractBaseParser
         $newsPostItemDTOList = $this->parseNewsPostContent($contentCrawler, $previewNewsItem);
 
         return $this->factoryNewsPost($previewNewsItem, $newsPostItemDTOList);
+    }
+
+    protected function getImageLinkFromNode(DOMElement $node): string
+    {
+        if ($node->hasAttribute('srcset')) {
+            $srcset = $node->getAttribute('srcset');
+            $parts = explode(', ', $srcset);
+            $maxSize = null;
+            $maxSizeSrc = null;
+            foreach ($parts as $srcString) {
+                $delimiterPosition = mb_strrpos($srcString, ' ');
+                $size = (int)mb_substr($srcString, $delimiterPosition);
+                if ($maxSize < $size) {
+                    $maxSize = $size;
+                    $maxSizeSrc = mb_substr($srcString, 0, $delimiterPosition);
+                }
+            }
+
+            if ($maxSizeSrc !== '' && $maxSizeSrc !== null) {
+                return $maxSizeSrc;
+            }
+        }
+
+        return $node->getAttribute('src');
     }
 }
